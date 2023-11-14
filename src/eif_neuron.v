5492e0549167616c33608757a1407b0aa61ef70c
mountains-high <kembay.assel@gmail.com>
2023-11-13 18:09:36 -0800

    modified eif neuron

--- src/eif_neuron.v
+++ src/eif_neuron.v
@@ -1,45 +1,20 @@
-
-/*
-    First-order integrate-and-fire neuron model.
-    Input is assumed to be a current injection.
-    
-    For :math:`U[T] > U_{\\rm thr} ⇒ S[T+1] = 1`.
-
-    If `reset_mechanism = "subtract"`, then :math:`U[t+1]` will have
-    `threshold` subtracted from it whenever the neuron emits a spike:
-
-    .. math::
-
-            U[t+1] = U[t] + I_{\\rm in}[t+1] - RU_{\\rm thr}
-
-    If `reset_mechanism = "zero"`, then :math:`U[t+1]` will be set to `0`
-    whenever the neuron emits a spike:
-
-    .. math::
-
-            U[t+1] = U[t] + I_{\\rm syn}[t+1] - R(U[t] + I_{\\rm in}[t+1])
-
-      :math:`I_{\\rm in}` - Input current
-      :math:`U` - Membrane potential
-      :math:`U_{\\rm thr}` - Membrane threshold
-      :math:`R` - Reset mechanism: if active, :math:`R = 1`, otherwise \
-      :math:`R = 0`
-    
-*/
 `default_nettype none
 
 module eif_neuron ( 
     input wire [7:0] current,
     input wire       clk,
     input wire       rst_n,
-    output wire      spike,
+
+    input wire Delta_T = 1.0,    // Sharpness parameter
+    input wire tau = 1.0,         // Membrane time constant
+
+    output reg       spike,
     output reg [7:0] state
 );
     reg  [7:0] threshold;
-    reg  [7:0] next_state; // Change from wire to reg
-
-    parameter Delta_T = 1.0;    // Sharpness parameter
-    parameter tau = 1.0;        // Membrane time constant
+    reg  [7:0] u_rest = 50; // Set the resting potential value, adjust as needed
+    reg  [7:0] theta_rh;    // Add missing declaration
+    reg  [7:0] next_state;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
@@ -50,17 +25,13 @@ module eif_neuron (
         end
     end
 
-    // Exponential Integrate-and-Fire model differential equation
-    always @* begin
+    always @(posedge clk) begin
         if (state >= threshold) begin
-            next_state = 0; // Reset the membrane potential
-            spike = 1;      // Generate spike
+            next_state = u_rest; // Reset the membrane potential to resting potential
+            spike = 1;           // Generate spike
         end else begin
-            next_state = state - (state - u_rest) + Delta_T * exp((state - theta_rh) / Delta_T) + current * tau; // Equation (5.6)
-            spike = 0;      // No spike
+            next_state = state + Delta_T * exp((state - theta_rh) / Delta_T) + current * tau; // Exponential Integrate-and-Fire equation
+            spike = 0;           // No spike
         end
     end
-
 endmodule
-
-
