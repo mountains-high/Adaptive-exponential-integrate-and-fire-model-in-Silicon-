127d3704e132f0aea525b788c1ed6988cb8375c1
mountains-high <kembay.assel@gmail.com>
2023-11-13 18:38:13 -0800

    modified eif

--- src/eif_neuron.v
+++ src/eif_neuron.v
@@ -1,38 +1,47 @@
 `default_nettype none
-`include "math.v"
+
 
 module eif_neuron ( 
+
     input wire [7:0] current,
     input wire       clk,
     input wire       rst_n,
-
-    input wire Delta_T = 1.0,    // Sharpness parameter
-    input wire tau = 1.0,         // Membrane time constant
-
-    output reg       spike,
+    output wire      spike,
     output reg [7:0] state
 );
     reg  [7:0] threshold;
-    reg  [7:0] u_rest = 50; // Set the resting potential value, adjust as needed
-    reg  [7:0] theta_rh;    // Add missing declaration
-    reg  [7:0] next_state;
+    wire [7:0] next_state;
+    reg        spike_history;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             state <= 0;
             threshold <= 200;
+            spike_history <= 0;
         end else begin
             state <= next_state;
         end
     end
 
+    // Spiking logic for IF
+    assign spike = (state >= threshold);
+    
+    // Adaptive threshold update based on spiking history
     always @(posedge clk) begin
-        if (state >= threshold) begin
-            next_state = u_rest; // Reset the membrane potential to resting potential
-            spike = 1;           // Generate spike
+        if (!rst_n) begin
+            spike_history <= 0;
         end else begin
-            next_state = state + Delta_T * exp((state - threshold) / Delta_T) + current * tau; // Exponential Integrate-and-Fire equation
-            spike = 0;           // No spike
+            spike_history <= spike;
+            // Adjust the threshold based on spiking history
+            if (spike_history) begin
+                threshold <= threshold - 10; // You can adjust the update logic
+            else
+                threshold <= threshold + 1;  // You can adjust the update logic
+            end
         end
     end
+
+    // Update the membrane potential
+    assign next_state = (spike ? 0 : (state + current)) - (spike ? threshold : 0);
+
 endmodule
